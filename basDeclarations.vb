702107144aefcdd96fbee773dba341f141af1942
Edson Armando <36713890+SistemaRayoXP@users.noreply.github.com>
2018-12-26 22:14:12 -0600

    Delete basDeclarations.vb

--- basDeclarations.vb
+++
@@ -1,5 +0,0 @@
-Option Strict Off
-Option Explicit On
-Module basDeclarations
-	Declare Function SetParent Lib "user32" (ByVal hWndChild As Integer, ByVal hWndNewParent As Integer) As Integer
-End Module
